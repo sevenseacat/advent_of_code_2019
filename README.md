53e69ba209a3ed3687f409023b02953b426151f2
Rebecca Le <traybaby@gmail.com>
2019-12-01 14:50:10 +0800

    Day 1, part 2

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ If you're curious how long each of my solutions roughly takes to run.
 
 ```
 Name                    ips        average  deviation         median         99th %
-day 1, part 1       10.75 K       93.02 μs   ±590.57%          68 μs         296 μs
+day 1, part 1       14.52 K       68.88 μs    ±52.02%       57.98 μs      151.98 μs
+day 1, part 2        9.81 K      101.89 μs    ±23.14%       91.98 μs      174.98 μs
 ```
--- lib/day_1.ex
+++ lib/day_1.ex
@@ -5,6 +5,12 @@ defmodule Day1 do
     |> Enum.sum()
   end
 
+  def part2(input) do
+    input
+    |> Enum.map(&fuel_mass_with_extra/1)
+    |> Enum.sum()
+  end
+
   def parse_input(data) do
     data
     |> String.trim()
@@ -27,10 +33,21 @@ defmodule Day1 do
   """
   def fuel_mass(num), do: div(num, 3) - 2
 
+  @doc """
+  iex> Day1.fuel_mass_with_extra(100756)
+  50346
+  """
+  def fuel_mass_with_extra(num, sum \\ 0) do
+    needed = fuel_mass(num)
+
+    if needed >= 0, do: fuel_mass_with_extra(needed, sum + needed), else: sum
+  end
+
   def bench do
     Benchee.run(
       %{
-        "day 1, part 1" => fn -> Advent.data(1) |> parse_input |> part1() end
+        "day 1, part 1" => fn -> Advent.data(1) |> parse_input |> part1() end,
+        "day 1, part 2" => fn -> Advent.data(1) |> parse_input |> part2() end
       },
       Application.get_env(:advent, :benchee)
     )
